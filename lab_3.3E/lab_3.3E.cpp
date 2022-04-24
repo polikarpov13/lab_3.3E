676948fcf9a887bb2c6e8a41a7650c21fe360f73
{Vlad} <{vladpolikarpovvl@gmail.com}>
2022-04-24 14:32:55 +0300

    first commit

---
+++ lab_3.3E/lab_3.3E.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "Date.h"
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Date A, B, C, D;
+
+    cout << " Enter A: " << endl;
+    cin >> A;
+
+    cout << " Enter B: " << endl;
+    cin >> B;
+
+    cout << " A > B " << endl;
+    if (A > B)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    cout << " Enter C: " << endl;
+    cin >> C;
+
+    cout << " Enter D: " << endl;
+    cin >> D;
+
+    cout << " C > D " << endl;
+    if (C > D)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    cout << " Кількість елементів класу Date : " << Object::Count() << endl;
+
+}
